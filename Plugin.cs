995bcfcc81ef604bd57bde919d62c7f1f96c73de
ciperuoy <ciperuoy@gmail.com>
2025-07-08 22:09:23 -0400

    v1

---
+++ Plugin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using BepInEx;
+using UnityEngine;
+using Utilla;
+
+namespace GorillaPlaytime
+{
+    [BepInPlugin("ciperuoy.gorillaplaytime", "GorillaPlaytime", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public string TimeElapsed = "";
+        Stopwatch watch = new Stopwatch();
+        public void Start()
+        {
+            GorillaTagger.OnPlayerSpawned(Init);
+        }
+        
+        public void Init()
+        {
+            watch.Start();
+        }
+
+        public void Update()
+        {
+            TimeElapsed = watch.Elapsed.ToString(@"hh\:mm\:ss");
+        }
+
+        public void OnGUI() => GUI.Label(new Rect(10, 10, 200, 20), TimeElapsed);
+    }
+}
