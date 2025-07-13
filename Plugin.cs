4281e1cc71b5fa5b76a84771d262555639fb8758
E14O <158614593+E14O@users.noreply.github.com>
2025-07-13 19:04:05 +0100

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Diagnostics;
 using BepInEx;
 using UnityEngine;
@@ -8,25 +9,27 @@ namespace GorillaPlaytime
     [BepInPlugin("ciperuoy.gorillaplaytime", "GorillaPlaytime", "1.0.1")]
     public class Plugin : BaseUnityPlugin
     {
-        Stopwatch watch = new Stopwatch();
-        public bool gui = true;
-        public string TimeElapsed = "";
+        private Stopwatch _watch;
+        private bool _showGui = true;
+        private string _timeElapsed = "";
 
-        public void Awake() => 
-            watch.Start();
-       
-        public void Update()
+        private void Awake()
         {
-            TimeElapsed = watch.Elapsed.ToString(@"hh\:mm\:ss");
-
-            if (Keyboard.current.tabKey.wasPressedThisFrame)
-                gui = !gui;
+            _watch = new Stopwatch();
+            _watch.Start();
         }
 
-        public void OnGUI()
+        private void Update()
         {
-            if (gui)
-                GUI.Label(new Rect(10, 10, 200, 20), TimeElapsed);
+            _timeElapsed = _watch.Elapsed.ToString(@"hh\:mm\:ss");
+            if (Keyboard.current != null && Keyboard.current.tabKey.wasPressedThisFrame)
+                _showGui = !_showGui;
+        }
+
+        private void OnGUI()
+        {
+            if (_showGui)
+                GUI.Label(new Rect(10, 10, 200, 20), $"Playtime: {_timeElapsed}");
         }
     }
 }
